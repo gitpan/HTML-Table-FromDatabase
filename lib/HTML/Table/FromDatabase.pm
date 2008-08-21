74585f0c50b78aa1fd37a1cdfc369228c45c60d5
David Precious <davidp@preshweb.co.uk>
2008-08-21 22:19:33 +0000

    Import of BIGPRESH/HTML-Table-FromDatabase-0.02 from CPAN.
    gitpan-cpan-distribution: HTML-Table-FromDatabase
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BIGPRESH/HTML-Table-FromDatabase-0.02.tar.gz
    gitpan-cpan-author:       BIGPRESH
    gitpan-cpan-maturity:     released

--- lib/HTML/Table/FromDatabase.pm
+++ lib/HTML/Table/FromDatabase.pm
@@ -4,10 +4,10 @@ use HTML::Table;
 use 5.005000;
 use strict;
 use base qw(HTML::Table);
+use vars qw($VERSION);
+$VERSION = '0.02';
 
-our $VERSION = '0.01';
-
-# $Id: FromDatabase.pm 411 2008-08-20 22:38:43Z davidp $
+# $Id: FromDatabase.pm 424 2008-08-21 22:00:46Z davidp $
 
 =head1 NAME
 
@@ -48,7 +48,9 @@ so you can use all the usual HTML::Table features.
 
 Constructor method - consult L<HTML::Table>'s documentation, the only
 difference here is the addition of the required I<-sth> parameter which
-should be a DBI statement handle.
+should be a DBI statement handle, and the optional I<-callbacks> parameter
+which specifies callbacks/transformations which should be applied as the
+table is built up (see the callbacks section below).
 
 =cut
 
@@ -56,15 +58,23 @@ sub new {
     my $class = shift;
     
     my %flags = @_;
-    my $self = HTML::Table->new(@_);
+    my $sth = delete $flags{-sth};
     
-    
-    my $sth = $flags{-sth};
     if (!$sth || !ref $sth || !$sth->isa('DBI::st')) {
         warn "HTML::Table::FromDatabase->new requires the -sth argument,"
             ." which must be a valid DBI statement handle.";
         return;
     }
+
+    my $callbacks = delete $flags{-callbacks};
+    if ($callbacks && ref $callbacks ne 'ARRAY') {
+        warn "Unrecognised -callbacks parameter; "
+            ."expected a arrayref of hashrefs";
+        return;
+    }
+    # Create a HTML::Table object, passing along any other options we were
+    # given:
+    my $self = HTML::Table->new(%flags);
     
     # Find the names;
     my @columns = @{ $sth->{NAME} };
@@ -73,8 +83,33 @@ sub new {
     $self->setSectionRowHead('thead', 0, 1);
     
     # Add all the rows:
-    while (my @data = $sth->fetchrow_array) {
-        $self->addRow(@data);
+    while (my $row = $sth->fetchrow_hashref) {
+        my @fields;
+        for my $column (@columns) {
+            my $value = $row->{$column};
+
+            # If we have a callbck to perform for this field, do it:
+            for my $callback (@$callbacks) {
+                # See what we need to match against, and if it matches, call
+                # the specified transform callback to potentially change the
+                # value.
+                if (exists $callback->{column}) {
+                    if (_callback_matches($callback->{column}, $column)) {
+                        $value = _perform_callback($callback, $column, $value);
+                    }
+                }
+                if (exists $callback->{value}) {
+                    if (_callback_matches($callback->{value}, $value)) {
+                        $value = _perform_callback($callback, $column, $value);
+                    }
+                }
+            }
+            
+            # Add this field to the list to deal with:
+            push @fields, $value;
+        }
+        
+        $self->addRow(@fields);
     }
     
     # All done, re-bless into our class and return
@@ -82,15 +117,101 @@ sub new {
     return $self;
 };
 
+# Abstract out the different kind of matches (regexp, coderef or straight
+# scalar)
+sub _callback_matches {
+    my ($match, $against) = @_;
+    if (ref $match eq 'Regexp') {
+        return $against =~ /$match/;
+    } elsif (ref $match eq 'CODE') {
+        return $match->($against);
+    } elsif (ref $match) {
+        # A reference to something we don't understand:
+        warn "Unrecognised callback match [$match]";
+        return;
+    } else {
+        # Must be a straight scalar
+        return $match eq $against;
+    }
+}
+
+# A callback spec matched, so perform any callback it requests, and apply
+# any transformation it described:
+sub _perform_callback {
+    my ($callback, $column, $value) = @_;
+
+    # Firstly, if there's a callback to perform, we call it (but don't
+    # care what it returns):
+    if (exists $callback->{callback} and ref $callback->{callback} eq 'CODE')
+    {
+        $callback->{callback}->($value, $column);
+    }
+
+    # Now, look for a transformation we might have to perform:
+    if (!exists $callback->{transform}) {
+        # We don't have a transform to perform, so just return the value
+        # unchanged:
+        return $value;
+    }
+    if (ref $callback->{transform} ne 'CODE') {
+        warn "Unrecognised transform action";
+        return $value;
+    }
+
+    # OK, apply the transformation to the value:
+    return $callback->{transform}->($value);
+}
+
 1;
 __END__;
 
 =back
 
+=head1 CALLBACKS
+
+You can pass an arrayref of hashrefs describing callbacks to be performed as
+the table is built up, which can modify the data before the table is produced.
+
+This can be very useful; one example use-case would be turning the values in
+a column which contains URLs into clickable links:
+
+ my $table = HTML::Table::FromDatabase->new(
+    -sth => $sth,
+    -callbacks => [
+        {
+            column => 'url',
+            transform => sub { $_ = shift; qq[<a href="$_">$_</a>]; },
+        },
+    ],
+ );
+
+You can match against the column name using a key named column in the hashref
+(as illustrated above) or against the actual value using a key named value.
+
+You can pass a straight scalar to compare against, a regex (using qr//), or
+a coderef which will be executed to determine if it matches.
+
+Another example - displaying all numbers to two decimal points:
+
+ my $table = HTML::Table::FromDatabase->new(
+    -sth => $sth,
+    -callbacks => [
+        {
+            value => qr/\d+/,
+            transform => sub { return sprintf '%.2f', shift },
+        },
+    ],
+ );
+
+It is hoped that this facility will allow the easyness of quickly creating
+a table to still be retained, even when you need to do things with the data
+rather than just displaying it exactly as it comes out of the database.
 =head1 AUTHOR
 
 David Precious, E<lt>davidp@preshweb.co.ukE<gt>
 
+Feel free to contact me if you have any comments, suggestions or bugs to
+report.
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -99,4 +220,3 @@ Copyright (C) 2008 by David Precious
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
 at your option, any later version of Perl 5 you may have available.
-
---
+++ t/mock.t
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+# $Id: mock.t 421 2008-08-21 21:45:06Z davidp $
+# Put HTML::Table::FromDatabase through its paces, using Test::MockObject
+# to provide a fake statement handle which provides known data.
+
+use strict;
+use Test::More;
+use HTML::Table::FromDatabase;
+
+eval "use Test::MockObject";
+plan skip_all => "Test::MockObject required for mock testing"
+    if $@;
+
+# OK, we've got Test::MockObject, so we can go ahead:
+plan tests => 7;
+
+# Easy test: get a mock statement handle, and check we can make a table:
+my $mock = mocked_sth();
+
+
+my $table = HTML::Table::FromDatabase->new( -sth => $mock );
+
+ok($table, 'Seemed to get a table back');
+isa_ok($table, 'HTML::Table', 'We got something that ISA HTML::Table');
+my $html = $table->getTable;
+warn $html;
+ok($html =~ m{<th>Col1</th>}, 'Table contains one of the known column names');
+ok($html =~ m{<td>R1C1</td>}, 'Table contains a known field value');
+
+# now, test transformations:
+$mock = mocked_sth();
+$table = HTML::Table::FromDatabase->new(
+    -sth => $mock,
+    -callbacks => [
+        {
+            column => qr/Col[12]/,
+            transform => sub { "RE_T" },
+        },
+        {
+            column => 'Col3',
+            transform => sub { "Plain_T" },
+        },
+        {
+            value => 'R2C4',
+            transform => sub { $_ = shift; s/R\dC\d/value_T/; $_ },
+        },
+    ],
+);
+$html = $table->getTable;
+warn $html;
+ok($html =~ m{<td>RE_T</td><td>RE_T</td>},
+    'Callback regexp-matching column transformed OK');
+ok($html =~ m{<td>Plain_T</td>},
+    'Callback plain-matching column transformed OK');
+ok($html =~ m{<td>value_T</td>}, 'Callback matching cell value transform OK');
+
+
+# Returns a make-believe statement handle, which should behave just like
+# a real one would, returning known data to test against.
+sub mocked_sth {
+    # Create a make-believe statement handle:
+    my $mock = Test::MockObject->new();
+    $mock->set_isa('DBI::st');
+
+    # Make it behave as we'd expect:
+    $mock->{NAME} = [ qw(Col1 Col2 Col3 Col4) ];
+    
+    #$mock->mock('fetchrow_hashref', sub { pop @{ shift->{_test_rows} } });
+    $mock->set_series('fetchrow_hashref', 
+        { Col1 => 'R1C1', Col2 => 'R1C2', Col3 => 'R1C3', Col4 => 'R1C4' },
+        { Col1 => 'R2C1', Col2 => 'R2C2', Col3 => 'R2C3', Col4 => 'R2C4' },
+        { Col1 => 'R3C1', Col2 => 'R3C2', Col3 => 'R3C3', Col4 => 'R3C4' },
+    );
+}
